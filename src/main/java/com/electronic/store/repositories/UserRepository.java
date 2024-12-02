fb7dca2790f8638f870e16c79239c54dbd7bfc69
rupeshk540 <rrupesh80844@gmail.com>
2024-12-02 08:23:06 +0530

    Initial commit-User module created

---
+++ src/main/java/com/electronic/store/repositories/UserRepository.java
@@ -0,0 +1,17 @@
+package com.electronic.store.repositories;
+
+import com.electronic.store.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String>
+{
+    //custom(finder) methods
+   Optional <User> findByEmail(String email);
+
+   Optional<User> findByEmailAndPassword(String email,String password);
+
+   List<User> findByNameContaining(String keywords);
+}
